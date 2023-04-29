6737e4c4507d4d9c1e5a58209380f9fc7d515315
Crowdbotics <team@crowdbotics.com>
2023-04-29 18:55:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mindmate-40346",
+  url: "https://mindmate-40346.botics.co",
+  api: "https://mindmate-40346.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
